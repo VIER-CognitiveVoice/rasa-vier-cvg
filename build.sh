0570dbb91b2a499224ccf80bd7c4be144770aeba
Phillip Schichtel <phillip.schichtel@vier.ai>
2022-09-27 11:27:22 +0200

    fully rename and deploy

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Merge apis/__init__.py
 version="${1:-"0.1.0"}"
+# shellcheck disable=SC2016
 VERSION="$version" envsubst '$VERSION' < setup.py.in > "setup.py"
 
-cd "$output_folder"
 python setup.py sdist
 python setup.py bdist_wheel
 
