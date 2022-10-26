8ad423e25a1e712769ee3698b89f381df57dd0a4
Phillip Schichtel <phillip.schichtel@vier.ai>
2022-10-26 13:44:32 +0200

    try a different build approach

--- build.sh
+++ build.sh
@@ -2,7 +2,22 @@
 
 set -eu
 
-export VERSION="${1:-}"
+if [ -e build ]
+then
+  rm -R build
+fi
+
+if [ -e dist ]
+then
+  rm -R dist
+fi
+
+version="${1:-}"
+if [ -n "$version" ]
+then
+  sed -i 's/version="9.9.9"/version="'"$version"'"/' setup.py
+fi
+
 python setup.py sdist
 python setup.py bdist_wheel
 
